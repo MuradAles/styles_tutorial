f91728aa98d696434086daf8769904cf2d9719b1
MuradAles <muradaleskerov99@gmail.com>
2023-06-30 16:55:41 -0400

    small changes

--- styles_tutorial/src/components/PageStructure.js
+++ styles_tutorial/src/components/PageStructure.js
@@ -1,28 +1,79 @@
 // html5 page structure
+import "./PageStructure.css"
 function PageStructure() {
     return (
         <div className="Page_Structure">
-            <header className="S_header">
-                Header
-            </header>
-            <nav className="S_nav">
-                Navigation
-            </nav>
-            <article className="S_article">
-                Body
-                <section className="S_section">
-                    Section 1
-                </section>
-                <section className="S_section">
-                    Section 2
-                </section>
-            </article>
-            <aside>
-                Aside
-            </aside>
-            <footer className="S_footer">
-                Footer
-            </footer>
+            <ol className="Carousel_containter">
+                <li id="slide_1" className="Carousel_box">
+                    <header className="S_header">
+                        Header 1
+                    </header>
+                    <nav className="S_nav">
+                        Navigation
+                    </nav>
+                    <article className="S_article">
+                        Body
+                        <section className="S_section">
+                            Section 1
+                        </section>
+                        <section className="S_section">
+                            Section 2
+                        </section>
+                    </article>
+                    <aside>
+                        Aside
+                    </aside>
+                    <footer className="S_footer">
+                        Footer
+                    </footer>
+                </li>
+                <li id="slide_2" className="Carousel_box">
+                    <header className="S_header">
+                        Header 2
+                    </header>
+                    <nav className="S_nav">
+                        Navigation
+                    </nav>
+                    <article className="S_article">
+                        Body
+                        <section className="S_section">
+                            Section 1
+                        </section>
+                        <section className="S_section">
+                            Section 2
+                        </section>
+                    </article>
+                    <aside>
+                        Aside
+                    </aside>
+                    <footer className="S_footer">
+                        Footer
+                    </footer>
+                </li>
+                <li id="slide_3" className="Carousel_box">
+                    <header className="S_header">
+                        Header 3
+                    </header>
+                    <nav className="S_nav">
+                        Navigation
+                    </nav>
+                    <article className="S_article">
+                        Body
+                        <section className="S_section">
+                            Section 1
+                        </section>
+                        <section className="S_section">
+                            Section 2
+                        </section>
+                    </article>
+                    <aside>
+                        Aside
+                    </aside>
+                    <footer className="S_footer">
+                        Footer
+                    </footer>
+                </li>
+            </ol>
         </div>
     )
 }
