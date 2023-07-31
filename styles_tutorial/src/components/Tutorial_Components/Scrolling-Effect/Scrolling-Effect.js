87481082adf359890406f8b5bfae93ebf92db2b5
MuradAles <muradaleskerov99@gmail.com>
2023-07-31 18:38:59 -0400

    small changes

--- styles_tutorial/src/components/Tutorial_Components/Scrolling-Effect/Scrolling-Effect.js
+++ styles_tutorial/src/components/Tutorial_Components/Scrolling-Effect/Scrolling-Effect.js
@@ -1,19 +1,24 @@
 import "./Scrolling-Effect.css"
 export default function Scrolling_Effect() {
+    const background_svg = (
+        <svg className="APP-BG_Sky" height="200px" width="200px" viewBox="0 0 1000 1000">
+            <defs>
+                <g id="sun">
+                    <rect
+                        className="Shape_rect"
+                        width="200"
+                        height="200" />
+                </g>
+            </defs>
+            <use href="#sun" x="400" y="400" />
+        </svg>
+    )
     return (
         <div className="SE_div">
-            <p>Parallax Effect </p>
-            <svg className="APP-BG_Sky" height="100%" width="100%" viewBox="0 0 1000 1000">
-                <defs>
-                    <g id="sun">
-                        <rect
-                            className="Shape_rect"
-                            width="200"
-                            height="200" />
-                    </g>
-                </defs>
-                <use href="#sun" x="400" y="400" />
-            </svg>
+            <div className="SE_div_element">
+                <h1>Parallax Effect</h1>
+                {background_svg}
+            </div>
         </div >
     )
 }
