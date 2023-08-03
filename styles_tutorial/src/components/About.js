f4cc8ac482a35b678e2877451de4170c03ae8720
MuradAles <muradaleskerov99@gmail.com>
2023-08-03 13:19:27 -0400

    small change

--- styles_tutorial/src/components/About.js
+++ styles_tutorial/src/components/About.js
@@ -1,9 +1,20 @@
 export default function About() {
+    const logo = (
+        <svg className="Shape_svg" width="300px" height="300px" viewBox="0 0 1000 1000">
+            <rect
+                className="Shape_rect"
+                x="400"
+                y="400"
+                width="200"
+                height="200" />
+        </svg>
+    )
     return (
         <div className="About">
             <p>This website is a tutorial that provides instructions on using CSS, HTML, and JavaScript to create stylish designs for your website. It also serves as a helpful cheatsheet for quick reference and reminders.</p>
             <p>Let me show you what kind of things you will learn here.</p>
             <p>Add animation of something cool</p>
+            {logo}
         </div>
     )
 }
--- styles_tutorial/src/components/Layouts/Header/Header.js
+++ styles_tutorial/src/components/Layouts/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css"
 export default function Header() {
     const logo = (
         <svg height="75px" width="75px" viewBox="0 0 100 100">
-            <rect x="40px" y="40px" fill="blue" height="20px" width="20px" />
+            <rect x="40" y="40" fill="blue" height="20px" width="20px" />
         </svg>
     )
     return (
